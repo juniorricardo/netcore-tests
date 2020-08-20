298b0cb64f88ca825967bdcf8766bd602b386d28
Junior Ricardo <junior.r.huamani@gmail.com>
2020-08-19 22:13:40 -0300

    test

---
+++ APIs/Poke.API/Middleware/IoC.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Poke.API.Helpers;
+using Poke.API.Interfaces;
+using Poke.Services;
+
+namespace Poke.API.Middleware
+{
+    public static class IoC
+    {
+        public static IServiceCollection AddDependency(this IServiceCollection services)
+        {
+            services.AddTransient<IPokemonService, PokemonService>();
+            services.AddTransient<IEnvironmentVariables, EnvironmentVariables>();
+            //services.AddTransient<IEnvironmentVariables, EnvironmentVariables>();
+            //services.AddTransient<IEnvironmentVariables, EnvironmentVariables>();
+            //services.AddTransient<IEnvironmentVariables, EnvironmentVariables>();
+            //services.AddTransient<IEnvironmentVariables, EnvironmentVariables>();
+            //services.AddTransient<IEnvironmentVariables, EnvironmentVariables>();
+            //services.AddTransient<IEnvironmentVariables, EnvironmentVariables>();
+            //services.AddTransient<IEnvironmentVariables, EnvironmentVariables>();
+
+            return services;
+        }
+    }
+}
--- APIs/Poke.API/Startup.cs
+++ APIs/Poke.API/Startup.cs
@@ -3,9 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Poke.API.Helpers;
-using Poke.API.Interfaces;
-using Poke.Services;
+using Poke.API.Middleware;
 
 namespace Poke.API
 {
@@ -23,8 +21,8 @@ namespace Poke.API
         {
             services.AddControllers();
 
-            services.AddTransient<IPokemonService, PokemonService>();
-            services.AddTransient<IEnvironmentVariables, EnvironmentVariables>();
+            IoC.AddDependency(services);
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
